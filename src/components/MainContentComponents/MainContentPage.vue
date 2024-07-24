832a5de55c0d99dc529989cbaa2ae05913f24285
punchalaken <penekkakti@gmail.com>
2024-07-24 23:33:13 +0300

    Correct main page

--- src/components/MainContentComponents/MainContentPage.vue
+++ src/components/MainContentComponents/MainContentPage.vue
@@ -1,22 +1,52 @@
 <script setup lang="ts">
-import MainServicesComponent from './MainServicesComponent.vue';
+import MainServicesComponent from './MainServicesComponent.vue'
 </script>
 
 <template>
     <section class="main">
         <div class="main__motto">
-            <div class="first">Забота о Вас и Вашей улыбке</div>
-            <div class="second">подарите её себе и миру.</div>
+            <div class="first">Забота о Вас</div>
+            <div class="second">и Вашей улыбке</div>
         </div>
         <img src="/src//assets/images/motto-photo.jpg" alt="Мое фото" class="main__photo" />
     </section>
 
-    <MainServicesComponent/>
+    <MainServicesComponent />
 </template>
 
 <style lang="scss" scoped>
-img {
-    width: 160px;
-    height: 160px;
+.main {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    max-width: 1200px;
+    width: 100%;
+    margin-top: 20px;
+    gap: 20px;
+
+    @media screen and (width<800px){
+        flex-direction: column;
+    }
+
+    .main__motto{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        font-family: 'Great Vibes';
+        font-size: clamp(0.3rem, 1rem + 1.5vw, 3rem);
+
+        .first{
+            padding-right: 80px;
+        }
+
+        .second{
+            padding-left: 80px;
+        }
+    }
+
+    img {
+        width: 360px;
+        height: 360px;
+    }
 }
 </style>
